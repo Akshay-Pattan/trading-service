f98bc62f13a895b792939208c9e83365be974463
Akshay Pattan <akshaypattan99@gmail.com>
2023-04-03 13:33:29 +0200

    switch to openapi generator to ease the end point implementation

--- src/main/java/com/db/corporate/bank/trading/service/v1/api/impl/HealthApiDelegateImpl.java
+++ src/main/java/com/db/corporate/bank/trading/service/v1/api/impl/HealthApiDelegateImpl.java
@@ -1,13 +1,17 @@
 package com.db.corporate.bank.trading.service.v1.api.impl;
 
-import com.db.corporate.bank.trading.service.v1.api.HealthApi;
+
+
+import com.db.corporate.bank.trading.service.v1.api.HealthApiDelegate;
 import com.db.corporate.bank.trading.service.v1.model.Health;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-public class HealthApiBusinessObjectImpl implements HealthApi {
-    @Override
-    public ResponseEntity<Health> healthGet() {
+@Service(value = "HealthApiDelegate")
+public class HealthApiDelegateImpl implements HealthApiDelegate {
+
+    public ResponseEntity<Health> getHealth() {
         return ResponseEntity.status(HttpStatus.OK).body(new Health().status(Health.StatusEnum.UP));
     }
 }
